973e8b564179e40eef8b4595ac586146a2b03532
khatkhat013 <khatkhat013@outlook.com>
2026-05-20 03:09:28 +0630

    chore: add npm install to deploy script

--- pull_deploy.mjs
+++ pull_deploy.mjs
@@ -3,7 +3,7 @@ import { Client } from 'ssh2';
 const conn = new Client();
 conn.on('ready', () => {
   console.log('Client :: ready');
-  conn.exec(`cd /var/www/burmese-digital-store && git pull origin main && npm run build && pm2 restart burmese-digital-store`, (err, stream) => {
+  conn.exec(`cd /var/www/burmese-digital-store && git pull origin main && npm install && npm run build && pm2 restart burmese-digital-store`, (err, stream) => {
     if (err) throw err;
     stream.on('close', (code, signal) => {
       console.log('Stream :: close :: code: ' + code);
